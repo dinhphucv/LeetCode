bb2fe51d636a58f5f14b99c464aa2b40c116acc1
dinhphucv <90453629+dinhphucv@users.noreply.github.com>
2025-02-10 18:45:35 -0800

    Update RemoveAllOccurrencesOfASubstring.java
    Using indexOf and initial the StringBuilder at start instead of append every new character and backward checking.

--- src/RemoveAllOccurrencesOfASubstring.java
+++ src/RemoveAllOccurrencesOfASubstring.java
@@ -6,13 +6,11 @@ public class RemoveAllOccurrencesOfASubstring {
     }
 
     public static String removeOccurrences(String s, String part) {
-        StringBuilder sb = new StringBuilder();
-
-        for (char aChar : s.toCharArray()) {
-            sb.append(aChar);
-            if (sb.length() >= part.length() && sb.substring(sb.length() - part.length(), sb.length()).equals(part)) {
-                sb.delete(sb.length() - part.length(), sb.length());
-            }
+        StringBuilder sb = new StringBuilder(s);
+        int startIndex = sb.indexOf(part);
+        while (startIndex != -1) {
+            sb.delete(startIndex, startIndex + part.length());
+            startIndex = sb.indexOf(part);
         }
 
         return sb.toString();
