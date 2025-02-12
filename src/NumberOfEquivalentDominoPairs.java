26df53c934a2dce65bbfb28ce4b2752e77b25756
dinhphucv <90453629+dinhphucv@users.noreply.github.com>
2025-02-12 00:18:22 -0800

    Update NumberOfEquivalentDominoPairs.java
    This solution replicates the use of 2 numbers as a single key min * 10 + max.
    The value is use for frequence.
    
    Runtime 11ms beats 73.66%
    Memory 52.63 beats 89.51%
    
    Also add comments.

--- src/NumberOfEquivalentDominoPairs.java
+++ src/NumberOfEquivalentDominoPairs.java
@@ -1,5 +1,4 @@
 import java.util.HashMap;
-import java.util.HashSet;
 
 public class NumberOfEquivalentDominoPairs {
     public static void main(String[] args) {
@@ -8,25 +7,24 @@ public class NumberOfEquivalentDominoPairs {
 
     public static int numEquivDominoPairs(int[][] dominoes) {
         int count = 0;
-        HashMap<Integer, HashMap<Integer, Integer>> firstSecondFrequencyMap = new HashMap<>();
-        int first;
-        int second;
-        int previousCount;
+        HashMap<Integer, Integer> keyFrequencies = new HashMap<>();
+        int first, second, key, previousFrequencies = 0;
+
         for (int[] domino : dominoes) {
             first = domino[0];
             second = domino[1];
+            // interpret 2 numbers as a single key (min * 10 + max) 36 instead of 3 and 6
+            key = Math.min(first, second) * 10 + Math.max(first, second);
 
-            if (!firstSecondFrequencyMap.containsKey(first)) {
-                firstSecondFrequencyMap.put(first, new HashMap<>());
-            }
-            firstSecondFrequencyMap.get(first).put(second, firstSecondFrequencyMap.get(first).getOrDefault(second, 0) + 1);
+            // get previous frequencies or default to 0;
+            previousFrequencies = keyFrequencies.getOrDefault(key, 0);
 
-            previousCount = firstSecondFrequencyMap.get(first).get(second) - 1;
-            if (firstSecondFrequencyMap.containsKey(second) && first != second) {
-                previousCount = previousCount + firstSecondFrequencyMap.get(second).getOrDefault(first, 0);
-            }
+            // add current count to previousFrequencies because if there is a new matching key, it create previousFrequencies new pairs.
+            // example, a key occur 9 times, if 10th key occur, it can pair with all 9 previous keys, so create 9 new pairs.
+            count = count + previousFrequencies;
 
-            count = count + previousCount;
+            // frequencies occur at the end of each iteration is use as value for the above key.
+            keyFrequencies.put(key, previousFrequencies + 1);
         }
 
         return count;
