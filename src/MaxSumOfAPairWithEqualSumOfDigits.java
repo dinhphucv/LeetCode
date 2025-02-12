9b32141d8cd3cf1f88a4348f1dc8f82f6d04e3ad
dinhphucv <90453629+dinhphucv@users.noreply.github.com>
2025-02-11 22:13:34 -0800

    Update MaxSumOfAPairWithEqualSumOfDigits.java
    The algorithm is still the same, just adding more comments to how the code is work. Also instead of reusing nums[i], this store the value in a variable to reuse it.
    
    Runtime 31ms beats 85.94%
    Memory 54.93MB beats 98.95%

--- src/MaxSumOfAPairWithEqualSumOfDigits.java
+++ src/MaxSumOfAPairWithEqualSumOfDigits.java
@@ -9,23 +9,32 @@ public class MaxSumOfAPairWithEqualSumOfDigits {
     public static int maximumSum(int[] nums) {
         int maximumSum = -1;
         int digitSum;
+        int num;
+
+        // this HashMap is used to map the digitSum to the index of the biggest number which the sum of all digit equal to digitSum.
         HashMap<Integer, Integer> digitSumMaxIndexMap = new HashMap<>();
         int currentMapIndex;
 
+        // looping through the array.
         for (int i = 0; i < nums.length; i++) {
-            digitSum = digitSum(nums[i]);
+            num = nums[i];
+            digitSum = digitSum(num);
+
+            // if the digitSum is not in the map, map the sum of all digit to i, which is the first number has that digitSum, so the biggest.
             if (!digitSumMaxIndexMap.containsKey(digitSum)) {
                 digitSumMaxIndexMap.put(digitSum, i);
                 continue;
             }
 
+            // save the currentMapIndex and check if current nums[i] is greater than previous biggest number with same digitSum and if update.
             currentMapIndex = digitSumMaxIndexMap.get(digitSum);
-            if (nums[i] > nums[currentMapIndex]) {
+            if (num > nums[currentMapIndex]) {
                 digitSumMaxIndexMap.put(digitSum, i);
             }
 
-            if (nums[currentMapIndex] + nums[i] > maximumSum) {
-                maximumSum = nums[currentMapIndex] + nums[i];
+            // check if the sum of num[i] and previous biggest number is greater than current maximumSum and update.
+            if (nums[currentMapIndex] + num > maximumSum) {
+                maximumSum = nums[currentMapIndex] + num;
             }
         }
 
@@ -34,6 +43,8 @@ public class MaxSumOfAPairWithEqualSumOfDigits {
 
     public static int digitSum(int num) {
         int sum = 0;
+
+        // modulo the number by 10 to get the last digit and divide by 10 to get rid of that digit to move to the next digit
         while (num > 0) {
             sum = sum + num % 10;
             num = num / 10;
